cb54b0e782511a5994fc035bc2654278dfb84deb
Damian Mirek <damian.mirek@gopos.pl>
2026-01-17 18:16:51 +0100

    Add validation for table's unique fields

--- src/main/java/com/booking/demo/error/GlobalExceptionHandler.java
+++ src/main/java/com/booking/demo/error/GlobalExceptionHandler.java
@@ -2,18 +2,25 @@ package com.booking.demo.error;
 
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    private static Map<String, String> constraintToColumn = Map.of(
+            "room.unique_name", "name",
+            "user.unique_email", "email"
+    );
+
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ApiError> globalEntityNotFoundHandler(EntityNotFoundException exception) {
         ApiError apiError = new ApiError("ENTITY_NOT_FOUND", exception.getMessage());
@@ -32,13 +39,34 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ApiError> globalMethodArgumentNotValidHandler(MethodArgumentNotValidException exception) {
         List<ValidationError> validationErrors = exception.getBindingResult().getFieldErrors().stream().map(fieldError -> {
             String field = fieldError.getField();
-            String message = fieldError.getDefaultMessage();
+            String value = fieldError.getDefaultMessage();
 
-            return new ValidationError(field, message);
+            return new ValidationError(field, value);
         }).toList();
 
         ApiError apiError = new ApiError("VALIDATION_ERROR", "Incorrect input provided", validationErrors);
 
         return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(apiError);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ApiError> globalDataIntegrityViolationHandler(DataIntegrityViolationException exception) {
+        Throwable error = exception.getCause().getCause();
+        ApiError apiError = new ApiError("DATA_INTEGRITY_ERROR", "Provided does not satisfy table's requirements");
+
+        if (error instanceof SQLException) {
+            int codeError = ((SQLException) error).getErrorCode();
+            if (codeError == 1062) {
+                String message = error.getMessage();
+                String[] messageParts = message.split(" ");
+                String field = messageParts[2].replaceAll("'", "");
+                String duplicatedValue = messageParts[5].replaceAll("'", "");
+                List<ValidationError> validationErrors = new ArrayList<>();
+                validationErrors.add(new ValidationError(constraintToColumn.getOrDefault(duplicatedValue, duplicatedValue), field));
+                apiError = new ApiError("DATA_UNIQUENESS_ERROR", "Unique field value already exists", validationErrors);
+            }
+        }
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(apiError);
     }
 }
--- src/main/java/com/booking/demo/error/ValidationError.java
+++ src/main/java/com/booking/demo/error/ValidationError.java
@@ -1,4 +1,4 @@
 package com.booking.demo.error;
 
-public record ValidationError(String field, String message) {
+public record ValidationError(String field, String value) {
 }
